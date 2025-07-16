731b886556cb84264df7373190ada14752b3fe27
fionnuala560 <fionnuala560@gmail.com>
2025-07-16 15:14:38 +0100

    Fixed refresh

--- src/spotify_auth/auth.py
+++ src/spotify_auth/auth.py
@@ -8,15 +8,15 @@ redirect_uri = st.secrets["spotify"]["redirect_uri"]
 scope = "user-library-read user-read-recently-played user-top-read"
 
 def authenticate_spotify():
-    if "token_info" not in st.session_state:
-        sp_oauth = SpotifyOAuth(
-            client_id=client_id,
-            client_secret=client_secret,
-            redirect_uri=redirect_uri,
-            scope=scope,
-            cache_path=".cache"
-        )
+    sp_oauth = SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        scope=scope,
+        cache_path=".cache"
+    )
 
+    if "token_info" not in st.session_state:
         token_info = sp_oauth.get_cached_token()
         if not token_info:
             auth_url = sp_oauth.get_authorize_url()
@@ -36,13 +36,13 @@ def authenticate_spotify():
 
     token_info = st.session_state.get("token_info")
 
-    if sp_oauth.is_token_expired(token_info):
+    # Refresh token if expired
+    if token_info and sp_oauth.is_token_expired(token_info):
         token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
         st.session_state.token_info = token_info
 
-    if token_info and not SpotifyOAuth(client_id, client_secret, redirect_uri).is_token_expired(token_info):
-        sp = spotipy.Spotify(auth=token_info["access_token"])
-        return sp
+    if token_info:
+        return spotipy.Spotify(auth=token_info["access_token"])
     else:
-        st.error("Token expired. Please reload the app and re-authenticate.")
+        st.error("Could not authenticate with Spotify.")
         return None
