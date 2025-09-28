4175dbd86e7347c35255dfde8cfbf5342ccc99e4
fionnuala560 <fionnuala560@gmail.com>
2025-09-28 15:14:38 +0100

    Removed hard coded environment variables

--- src/app/app.py
+++ src/app/app.py
@@ -30,9 +30,9 @@ def load_user(user_id):
     return users.get(user_id)
 
 # --- Spotify OAuth setup ---
-CLIENT_ID = "6b3b52af29e6458d8e0cd708d84d5159"
-CLIENT_SECRET = "b2ba9d919d0d40b4ae8dba1ff45307b8"
-REDIRECT_URI = "http://127.0.0.1:5000/callback"
+CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+REDIRECT_URI = os.environ.get("REDIRECT_URI")
 SCOPE = "user-top-read user-read-private"
 
 def get_sp_oauth():
